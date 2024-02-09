fd754843dec1da651a13cb4df6e99bfa7e0dc68e
Swen <swentemebel@outlook.com>
2024-02-09 16:27:02 +0100

    push all

--- src/components/weather.vue
+++ src/components/weather.vue
@@ -1,22 +1,43 @@
 <template>
-    <h1>Weather app</h1>
-    <input type="text" v-model.trim="placeName"> <button @click="ophalen(placeName)">Zoek</button>
-    <div v-if='theWeather || theWeather.length >= 1' class="">
-        {{ theWeather }}<br>
-        Name City: {{ theWeather.name }}<br>
-        Temperatuur: {{ theWeather.main.temp }}<br>
-        Gevoels temp: {{ theWeather.main.feels_like }} <br>
-        Regen verwacht: {{ theWeather.weather[0].description }} in {{ theWeather.rain }}
-        <img :src='getimgurl(theWeather.weather[0].icon)' alt="icoonweer">
+    <div class="bg-yellow-500 grid justify-center md:w-screen font-serif">
+        <div class="bg-orange-500 md:w-[50rem] mt-10 p-10 md:h-[10rem] md:grid md:justify-center md:p-2">
+            <h1 class="text-center text-2xl font-bold text-shadow-xl ">Weather app</h1>    
+            <input class='p-2 md:p-1 bg-green-200 md:mt-3 md:w-[40rem] rounded-l-xl md:rounded-xl text-center text-lg ' type="text" v-model.trim="placeName" placeholder="Search City"> 
+            <button class=' p-2 md:p-1 bg-green-300 md:mt-3 md:w-[40rem] rounded-r-xl md:rounded-xl text-center text-lg text-semibold ' @click="ophalen(placeName)">Zoek</button>
+        </div>
+        <!--<div v-if='!error()' class="">{{ errormsg }}</div>-->
+        <div v-if='theWeather || theWeather.length >= 1' class="bg-purple-300">
+            <div class="bg-yellow-700 grid justify-center m-5 md:px-5">
+                <div class="bg-blue-200 w-50">
+                    <div class="grid justify-center">
+                        <span class="text-3xl font-semibold mt-10">&#8451; {{ theWeather.main.temp }} </span>
+                        <span class="text-xl  font-semibold mt-10">{{ theWeather.name }}</span>
+                    </div>
+                
+                    <div class="flex justify-center">
+                        <div class="bg-sky-300 flex items-center justify-center md:w-[20rem] w-[10rem]">
+                            <span class="text-lg first-letter:capitalize font-medium">{{ theWeather.weather[0].description }}</span>
+                        </div>
+                        <div class="bg-orange-300">
+                            <img :src='getimgurl(theWeather.weather[0].icon)' alt="icoonweer">
+                        </div>
+                    </div>
+                </div>
+              
+              
+            </div>
+        </div>
     </div>
 </template>
 
 
 <script>
-   
+import mixins from '../mixins/mixins'; 
+
 export default {
     name: 'weatherApp',
-    
+    mixins: [mixins],
+
     methods:{
         async ophalen(location){
             console.log('Gezocht op plaats: ' + location  )
@@ -24,12 +45,7 @@ export default {
             console.log(this.$store.state.WeatherAPI.place)
         },
 
-        getimgurl(icon){
-            const imgUrl = 'https://openweathermap.org/img/wn/';
-            const sizeIcon = '@2x.png';
-            const urlIcon = imgUrl + icon + sizeIcon;
-            return urlIcon;
-        }
+      
     },
 
     computed:{
--- src/mixins/mixins.js
+++ src/mixins/mixins.js
@@ -2,12 +2,19 @@
 export default {
 
     methods: {
-        async getImgUrl(img){
+        async getImgUrl(img){ // maakt 
             console.log('getImgUrl() word aangeroepen vanuit mixins map en bestand. met de img ' + img );
             const { default: imageUrl } = await import(`@/assets/gerechtenimg/${img}`);
             return imageUrl;
         },
-   
+
+        // Functie die het icon van het de component weather maakt. 
+        getimgurl(icon){ // icon staat voor het icon benaming die hij uit de API haalt.
+            const imgUrl = 'https://openweathermap.org/img/wn/'; // url voor icon
+            const sizeIcon = '@2x.png'; //icon grote
+            const urlIcon = imgUrl + icon + sizeIcon; //voegt alles samen
+            return urlIcon;
+        },
 
     
     }
